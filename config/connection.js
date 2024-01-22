9cee1c2dcef2dda00e840abeaf0dc897b5b56f4e
Jay <j.thomastt1@gmail.com>
2024-01-21 19:41:33 -0600

    Finished Category API routes

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,16 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+// create connection to our db
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
+});
+
+/*
+const Sequelize = require('sequelize');
+
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
@@ -11,5 +21,5 @@ const sequelize = process.env.JAWSDB_URL
         decimalNumbers: true,
       },
     });
-
+*/
 module.exports = sequelize;
